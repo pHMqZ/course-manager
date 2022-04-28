f3ac8244d9bb02e033383ab15a6b08f2deec67d7
Phillip Marques <phillipmarq@hotmail.com>
2022-04-28 18:11:52 -0300

    Project adjustments

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { StarComponent } from './star/star.component';
+import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/course-list.component';
+import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Error404Compoment } from './error-404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Error404Compoment,
+    CourseInfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      
+      {
+        path:'**', component: Error404Compoment
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/courses/course-info.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Course } from "./course";
+import { CourseService } from "./course.service";
+
+@Component({
+    templateUrl: './course-info.component.html'
+})
+
+export class CourseInfoComponent implements OnInit{
+    
+    course: Course;
+    
+    
+
+    constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService, course: Course) {
+        this.course = course;
+    }
+
+    ngOnInit(): void {
+        this.course = this.courseService.retriveById(+this.activatedRoute.snapshot.paramMap.get('id'));
+    }
+
+    save(): void{
+        this.courseService.save(this.course);
+    }
+}
